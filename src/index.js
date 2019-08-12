15fc5add8558d87b754fc7933ab94e187dabcadf
skennel2 <skennel2@gmail.com>
2019-08-12 14:04:05 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import XmlData from './data.xml';
+import _ from 'lodash'
 
-function component() {
+function component(){
     const element = document.createElement('div');
-  
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;    
-
-    element.appendChild(myIcon);
-
-    console.log(XmlData);
 
     return element;
 }
-  
+
 document.body.appendChild(component());
