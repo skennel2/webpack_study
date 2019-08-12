d1b358fced5bb85d0aad50d7d0ddd6a0382a0423
skennel2 <skennel2@gmail.com>
2019-08-12 18:44:57 +0900

    dynamic import

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import _ from 'lodash';
 
-function component(){
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = "Click me and check the console";
-    btn.onclick = import('./print').then(func => {
-        console.log(func)
-        func.printMe();
+function getComponent(){
+    return import('./print').then(({default : func})=>{
+        const element = document.createElement('div');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        element.onclick = func;
+        return element;
     });
-
-    element.appendChild(btn);
-
-    return element;
 }
 
-document.body.appendChild(component());
+getComponent().then(component=> {
+    document.body.appendChild(component);
+});
+
