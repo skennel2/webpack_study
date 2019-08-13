aec8ed651db75a7271a1738152caf6d03e7e567a
skennel2 <skennel2@gmail.com>
2019-08-13 11:45:58 +0900

    eslint 적용

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component(){
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.onclick = Print;
-    return element;
+    return element;    
 }
 
 document.body.appendChild(component());
